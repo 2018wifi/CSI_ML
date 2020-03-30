c3aba453f1e86df5041cb25c8608f6d27ad87792
Whusober <2018302180106@whu.edu.cn>
2020-03-30 15:51:55 +0800

    Replace pcap_reader.py with parse_pcap.py. Improve main.py

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-import pcap_reader
+
 BW = 20  # 带宽
 NFFT = int(BW * 3.2)
 GESTURE_NUM = 3  # 手势数
-TRAIN_SIZE = 10000  # 训练集大小
-PCAP_SIZE = 1000  # 每个pcap包含的CSI数组个数
+TRAIN_SIZE = 45  # 训练集大小
+PCAP_SIZE = 10  # 每个pcap包含的CSI数组个数
 
 train_data = np.zeros((TRAIN_SIZE, PCAP_SIZE, NFFT))
 train_result = np.zeros(TRAIN_SIZE)
+
+
+for i in range(1, 16):
+    # 读取npy文件
+    temp_clap = np.load('data/clap' + str(i) + '.npy')
+    temp_gesture = np.load('data/gesture' + str(i) + '.npy')
+    temp_result = np.load('data/result' + str(i) + '.npy')
+
+    # CSI求模&&毛刺处理
+    for j in range(PCAP_SIZE):
+        for k in range(NFFT):
+            temp_clap[j][k] = abs(temp_clap[j][k])
+            temp_gesture[j][k] = abs(temp_gesture[j][k])
+            temp_result[j][k] = abs(temp_result[j][k])
+            if 29 <= abs(k - 32) <= 32 or k == 32:
+                temp_clap[j][k] = 0
+                temp_gesture[j][k] = 0
+                temp_result[j][k] = 0
+
+    # 加入学习队列
+    temp_clap = temp_clap.astype('float64')
+    temp_gesture = temp_gesture.astype('float64')
+    temp_result = temp_result.astype('float64')
+    train_data[i - 1] = temp_clap[0:PCAP_SIZE, :]
+    train_data[2 * (i - 1)] = temp_gesture[0:PCAP_SIZE, :]
+    train_data[3 * (i - 1)] = temp_result[0:PCAP_SIZE, :]
+    train_result[i - 1] = 0
+    train_result[2 * (i - 1)] = 1
+    train_result[3 * (i - 1)] = 2
+
+
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(PCAP_SIZE, NFFT)),
     keras.layers.Dense(128, activation='relu'),
@@ -18,5 +49,4 @@ model = keras.Sequential([
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
-model.fit(train_data, train_result, epoches=10)
-
+model.fit(train_data, train_result)
---
+++ parse_pcap.py
@@ -0,0 +1,66 @@
+import dpkt
+import struct
+import numpy as np
+
+BW = 20
+NFFT = int(BW * 3.2)
+
+def parse_udp(buffer):      # 解析udp包的二进制流
+    nbyte = int(len(buffer))        # 字节数
+    data = np.array(struct.unpack(nbyte * "B", buffer), dtype=np.uint8)
+
+    return data
+
+def read_header(data):  # 提取头信息
+    header = {}
+
+    header["magic_bytes"] = data[:4]
+    header["source_mac"] = data[4:10]
+    header["sequence_number"] = data[10:12]
+
+    coreSpatialBytes = int.from_bytes(data[12:14], byteorder="little")
+    header["core"] = [int(coreSpatialBytes & x != 0) for x in range(3)]
+    header["spatial_stream"] = [int(coreSpatialBytes & x != 0) for x in range(3, 6)]
+
+    header["channel_spec"] = data[14:16]
+    header["chip"] = data[18:20]
+
+    return header
+
+def read_csi(data):     # 提取CSI信息，并转换成矩阵
+    csi = np.zeros(NFFT, dtype=np.complex)
+    sourceData = data[18:]
+    sourceData.dtype = np.int16
+    csi_data = sourceData.reshape(-1, 2).tolist()
+
+    i = 0
+    for x in csi_data:
+        csi[i] = np.complex(x[0], x[1])
+        i += 1
+
+    return csi
+
+def parse_pcap(pcap_file, npy_file):        # 将源pcap文件转为numpy矩阵的二进制文件
+    f = open(pcap_file, 'rb')
+    pcap = dpkt.pcap.Reader(f)
+
+    matrix_list = []
+    for ts, buf in pcap:
+        if len(buf) != 316:
+            continue
+        data = parse_udp(buf[42:])
+        csi = read_csi(data)
+        matrix_list.append(csi)
+
+    matrix = np.array(matrix_list)
+    f.close()
+    with open(npy_file, 'wb+') as _:
+        pass
+    np.save(npy_file, matrix)
+
+if __name__ == '__main__':
+    for i in range(1, 16):
+        print("processing pcap: ", i)
+        parse_pcap('data_raw/clap' + str(i) + '.pcap', 'data/clap' + str(i) + '.npy')
+        parse_pcap('data_raw/gesture' + str(i) + '.pcap', 'data/gesture' + str(i) + '.npy')
+        parse_pcap('data_raw/result' + str(i) + '.pcap', 'data/result' + str(i) + '.npy')
